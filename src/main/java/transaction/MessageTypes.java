46f72b8b827c4d4e1a08734abd3c209452f6d38e
Scott Ames <scottaames@gmail.com>
2020-03-25 18:18:18 -0700

    added msg and lock types

--- src/main/java/transaction/MessageTypes.java
+++ src/main/java/transaction/MessageTypes.java
@@ -11,4 +11,10 @@ package transaction;
  */
 public interface MessageTypes {
     
+    public static final int OPEN_TRANSACTION = 1;
+    public static final int CLOSE_TRANSACTION = 2;
+    
+    public static final int READ_REQUEST = 3;
+    public static final int WRITE_REQUEST = 4;
+    
 }
