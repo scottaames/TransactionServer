14855cf20cd99a00d7d317d2131d47d1b80c2a30
Scott Ames <scottaames@gmail.com>
2020-03-29 16:51:32 -0700

    fixed transaction

--- src/main/java/client/TransactionServerProxy.java
+++ src/main/java/client/TransactionServerProxy.java
@@ -9,14 +9,9 @@ package client;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
 import transaction.MessageTypes;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import transaction.Message;
-import transaction.Transaction;
-import transaction.TransactionServer;
 
 import Logger.OutputLogger; 
 import static transaction.MessageTypes.READ_REQUEST;
