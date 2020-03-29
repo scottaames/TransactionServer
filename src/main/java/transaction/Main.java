44f1695f746d1dd913f4b6e72e6d50cfb6d98726
Scott Ames <scottaames@gmail.com>
2020-03-29 14:10:11 -0700

    added main runner file

---
+++ src/main/java/transaction/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transaction;
+
+import client.TransactionClient;
+
+/**
+ *
+ * @author scott
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        TransactionClient client = new TransactionClient(20, 8080);
+        TransactionServer server = new TransactionServer(10, true );
+        
+        server.run();
+        client.start();
+    }
+}
